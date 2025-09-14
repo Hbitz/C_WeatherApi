ba00d25a35366a593fc743fe637781814cecb80d
Hbitz <53080862+Hbitz@users.noreply.github.com>
2025-09-14 12:40:13 +0200

    Comment clarity

--- include/http.h
+++ include/http.h
@@ -3,6 +3,10 @@
 
 #include <stddef.h>
 
+// int used for loop counters and small values where negative matter
+// size_t (unsigned int) is specifically meant to represent sizes of objects in memory,
+// (array lengths, sizeof results, memory allocation sizes etc)
+// size_t's underlying type depends on the platform(whether its 32 or 64bit based)
 typedef struct {
     char *data;
     size_t size;
